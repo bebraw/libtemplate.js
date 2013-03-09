df2963e9a5c4345daa1e860ae9778cf5d45ba117
Juho Vepsalainen <bebraw@gmail.com>
2013-03-09 19:46:41 +0200

    Add various files to Bower ignore

--- dist/libtemplate.js
+++ dist/libtemplate.js
@@ -1,5 +1,5 @@
-/*! libtemplate - v0.2.1 - Juho Vepsalainen - MIT
- - 2013-03-07 */
+/*! libtemplate - v0.2.2 - Juho Vepsalainen - MIT
+ - 2013-03-09 */
 // TODO: replace this with your library
 function foo() {
     return 'bar';
